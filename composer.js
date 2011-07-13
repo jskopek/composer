fcec8f01692e2414dd18037fb52a4b174c946d49
Jean-Marc Skopek <jskopek@gmail.com>
2011-07-13 12:56:41 -0400

    Cleaned up composer

--- composer.js
+++ composer.js
@@ -1,7 +1,7 @@
 var composerItem = Backbone.Model.extend({
 	initialize: function() {
 		if( !this.get("id") && !this.get("type") ) {
-			alert("Missing `id` or `type` properties!");
+			throw("Missing `id` or `type` properties!");
 		}
 
 		var el = $("<div></div>");
@@ -10,7 +10,6 @@ var composerItem = Backbone.Model.extend({
 
 		var widget = this.get_widget();
 		widget.initialize.apply(this);
-		//this.collection.el.append(el);
 	},
 	get_widget: function() {
 		var widget = this.collection.widgets[this.get("type")];
@@ -25,36 +24,12 @@ var composerItem = Backbone.Model.extend({
 	}
 
 });
+
 var composerCollection = Backbone.Collection.extend({
 	model: composerItem,
 	widgets: {},
 	validation: {}
-	//initialize: function(a,b) {
-	//},
-	//add: function(a,b,c) {
-		 //}
 });
-var composer = function(el, data) {
-	this.collection = new composerCollection();
-
-	this.collection.el = el;
-	if( data ) {
-		this.collection.add(data);
-	}
-
-	//interfaces for collection tasks
-	this.add = function(data) {
-		this.collection.add(data);
-	};
-
-	//interfaces for adding widgets and validation
-	this.addWidget = function(type, fn) {
-		this.collection.widgets[type] = fn;
-	};
-	this.addValidation = function(type, fn) {
-		this.collection.validation[type] = fn;
-	};
-};
 
 (function($) {
 	$.fn.extend({
