36c304f244a3a717672fcdeb88bb3eead090f0a2
Jean-Marc Skopek <jskopek@gmail.com>
2011-07-13 11:35:59 -0400

    Set up validation functions

--- page.js
+++ page.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
 		"id": "username",
 		"type": "text",
 		"value": "jms",
-		"label": "Username"
+		"label": "Username",
+		"validation": ["not_empty", "longer_than"],
+		"longer_than": 4
 	};
 	var data2 = {
 		"id": "password",
@@ -30,6 +32,7 @@ $(document).ready(function() {
 
 	var c = new composer( $("#myform") );
 
+	//------ widgets -----
 	var defaultWidget = {
 		"initialize": function() {
 			var html = '';
@@ -65,7 +68,7 @@ $(document).ready(function() {
 			this.value( this.value() );
 		}
 	}));
-	//checkbox, radio, textarea, select, number, picker
+	//textarea, select, number, picker
 	c.addWidget("checkbox", $.extend({}, defaultWidget, {
 		"initialize": function() {
 			var html = '';
@@ -104,6 +107,106 @@ $(document).ready(function() {
 			return $(this.get("el")).find("input[selected]").attr("value");
 		}
 	}));
+	c.addWidget("textarea", {});
+	c.addWidget("select", {});
+	c.addWidget("picker", {});
+	c.addWidget("number", {});
+	c.addWidget("uploadify", {});
+
+	//----- validation ------
+	c.addValidation("num_greater_than", function(item_val) {
+		var c = parseInt( this.get("num_greater_than") );
+		if( isNaN(parseFloat(item_val)) || (parseFloat(item_val) <= c) ) {
+			return 'Value must be greater than ' + c;
+		} else {
+			return true;
+		}
+	});
+	c.addValidation("num_greater_than_equal", function(item_val) {
+		var c = parseInt( this.get("num_greater_than_equal") );
+		if( isNaN(parseFloat(item_val)) || (parseFloat(item_val) < c) ) {
+			return 'Value must be greater than or equal to ' + c;
+		} else {
+			return true;
+		}
+	});
+	c.addValidation("num_less_than", function(item_val) {
+		var c = parseInt( this.get("num_less_than") );
+		if( isNaN(parseFloat(item_val)) || (parseFloat(item_val) >= c) ) {
+			return 'Value must be less than ' + c;
+		} else {
+			return true;
+		}
+	});
+	c.addValidation("num_less_than_equal", function(item_val) {
+		var c = parseInt( this.get("num_less_than_equal") );
+		if( isNaN(parseFloat(item_val)) || (parseFloat(item_val) > c) ) {
+			return 'Value must be less than or equal to ' + c;
+		} else {
+			return true;
+		}
+	});
+	c.addValidation("not_empty", function(item_val) {
+		var valid = false;
+		if( typeof(item_val) == "string" ) {
+			//"value" == :), "    " == :(, "" == :(
+			if( item_val.trim() != "" ) {
+				valid = true;
+			}
+		} else if( typeof(item_val) == "object" ) {
+			//{0: "value 1", 1: "value 2"} == :), {0: "value 1"} == :), {0: "   "} == :(, {} == :(
+
+			//loop through items and return list with true/false based on if they have empty values
+			var results = _.map(item_val, function(item) { return item.trim() == ""; });
+			if( results.length > 0 && !_.include(results, true) ) {
+				valid = true;
+			}
+		}
+		
+		if( !valid ) {
+			return 'Value must not be empty';
+		} else {
+			return true;
+		}
+	});
+	c.addValidation("integer", function(item_val) {
+		if( parseInt(item_val) != (item_val-0) ) {
+			return 'Value must be an integer';
+		} else {
+			return true;
+		}
+	});
+	c.addValidation("number", function(item_val) {
+		if( parseFloat(item_val) != (item_val-0) ) {
+			return 'Value must be a number';
+		} else {
+			return true;
+		}
+	});
+	c.addValidation("longer_than", function(item_val) {
+		var c = parseInt( this.get("longer_than") );
+		if( item_val.length <= c ) {
+			return 'Value must be more than ' + c + ' characters long';
+		} else {
+			return true;
+		}
+	});
+	c.addValidation("shorter_than", function(item_val) {
+		var c = parseInt( this.get("shorter_than") );
+		if( item_val.length >= c ) {
+			return 'Value must be less than ' + c + ' characters long';
+		} else {
+			return true;
+		}
+	});
+	c.addValidation("email", function(item_val) {
+		var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+		if( !reg.test(item_val) ) {
+			return 'Value must be a valid email address';
+		} else {
+			return true;
+		}
+	});
 
 	c.add( dataset );
 })
